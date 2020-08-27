65e6e0e4c66b3726db06b0b0f6dc75ba9573cfdc
Reshmi3132 <70102871+Reshmi3132@users.noreply.github.com>
2020-08-27 18:43:59 +0530

    Update reshmi-botapp1.py

--- reshmi-botapp1.py
+++ reshmi-botapp1.py
@@ -20,19 +20,17 @@ def follower_count(user):
   emoji_numbers =  {0: "0️⃣", 1: "1️⃣", 2: "2️⃣", 3: "3️⃣",
                       4: "4️⃣", 5: "5️⃣", 6: "6️⃣", 7: "7️⃣", 8: "8️⃣", 9: "9️⃣"}
 
-  uf_split = [int(i) for i in str(user.follower_count)]
+  uf_split = [int(i) for i in str(user.followers_count)]
 
   emoji_followers = ''.join([emoji_numbers[j] for j in uf_split if j in emoji_numbers.keys()]) 
   return emoji_followers
 
 api = create_api()
 
-def main():
-  while True:
+while True:
     user = api.get_user('intdummy')
     api.update_profile(name=f'Reshmi|{follower_count(user)} Followers')
     print(f'Updating Twitter Name : Reshmi|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
-main()
 
